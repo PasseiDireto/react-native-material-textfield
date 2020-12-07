ed4e831edb00b12bb6759e7f96d181a01cae5604
acgoj <acgojunior@gmail.com>
2020-12-07 18:19:13 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import TextField from './src/components/field';
 import FilledTextField from './src/components/field-filled';
 import OutlinedTextField from './src/components/field-outlined';
+import Label from './src/components/label'
 
-export { TextField, FilledTextField, OutlinedTextField };
+export { TextField, FilledTextField, OutlinedTextField, Label };
